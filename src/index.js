3eddbe1aea41762ad1299db0338671e6dd8ead0f
gunnmgirl <gunnmgirl@gmail.com>
2020-12-29 09:06:39 +0100

    Adds health-check route

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import healthCheckRoutes from "./routes/healthCheckRoutes";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/healthCheck", healthCheckRoutes);
+
+app.use((error, req, res, next) => {
+  console.log("In error middleware", error);
+  res.status(error.statusCode).send(error.message);
+});
+
+app.listen(4000, () => console.log(`Running on port 4000`));
