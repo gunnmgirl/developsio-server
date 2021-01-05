11da4f2381d916bc31186d81c02a2bd2719c2fa6
gunnmgirl <gunnmgirl@gmail.com>
2021-01-05 15:24:28 +0100

    DEV-24 Implements sign up route and controller function

---
+++ src/people/peopleModel.js
@@ -0,0 +1,30 @@
+import Sequelize from "sequelize";
+
+import sequelize from "../database/connection";
+
+const People = sequelize.define(
+  "people",
+  {
+    firstName: { type: Sequelize.STRING, allowNull: false },
+    lastName: { type: Sequelize.STRING, allowNull: false },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    jobPosition: {
+      type: Sequelize.STRING,
+    },
+    country: { type: Sequelize.STRING(100), allowNull: false },
+    city: { type: Sequelize.STRING(50), allowNull: false },
+    streetAddress: { type: Sequelize.STRING(100), allowNull: false },
+    phoneNumber: { type: Sequelize.STRING(15), allowNull: false },
+    previousPositions: { type: Sequelize.TEXT, allowNull: false },
+    skype: { type: Sequelize.STRING },
+    imageUrl: { type: Sequelize.STRING },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default People;
