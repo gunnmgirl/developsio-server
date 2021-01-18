a136ac66bf4a3662464ee126fcb9a77413954f1a
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2021-01-18 15:18:34 +0100

    DEV-64 Updates database config for deployment to heroku (#12)
    Co-authored-by: Dzana Karamustafic <dzanakaramustafic@Dzanas-MacBook-Pro.local>

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,21 +2,21 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.USER,
+    username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
     dialect: "mysql",
   },
   test: {
-    username: process.env.USER,
+    username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
     dialect: "mysql",
   },
   production: {
-    username: process.env.USER,
+    username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
