591c80c0692374b801fe24fd05b493b2aa468802
Dzana Karamustafic <dzanakaramustafic@Dzanas-MacBook-Pro.local>
2021-01-18 21:34:31 +0100

    Updates connection string

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,6 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USERNAME,
-  process.env.PASSWORD,
-  {
-    host: process.env.HOST,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 sequelize.sync();
 
