3545ded2699cf6442e159deb0254074d93ddde4a
Dzana Karamustafic <dzanakaramustafic@Dzanas-MacBook-Pro.local>
2021-01-18 13:46:10 +0100

    DEV-64 Updates database config for deployment to heroku

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 const sequelize = new Sequelize(
   process.env.DATABASE,
-  process.env.USER,
+  process.env.USERNAME,
   process.env.PASSWORD,
   {
     host: process.env.HOST,
