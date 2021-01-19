6802a1d4e3a360e7f30331bcc1e5bc4defe5e761
Dzana Karamustafic <dzanakaramustafic@Dzanas-MacBook-Pro.local>
2021-01-19 09:29:18 +0100

    DEV-66 Updates sequelize connection for development env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    host: process.env.HOST,
+    username: process.env.USERNAME_DEV,
+    password: process.env.PASSWORD_DEV,
+    database: process.env.DATABASE_DEV,
+    host: process.env.HOST_DEV,
     dialect: "mysql",
   },
   test: {
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,20 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+let sequelize;
+
+if (process.env.NODE_ENV === "development") {
+  sequelize = new Sequelize(
+    process.env.DATABASE_DEV,
+    process.env.USERNAME_DEV,
+    process.env.PASSWORD_DEV,
+    {
+      host: process.env.HOST_DEV,
+      dialect: "mysql",
+    }
+  );
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+}
 
 sequelize.sync();
 
