46ad2d0d02eb95ab013ebe8bfc953608326cc16a
Dzana <gunnmgirl@gmail.com>
2021-01-26 16:38:06 +0100

    DEV-89 Updates get applicants route to return more data

--- src/applicants/applicantsController.js
+++ src/applicants/applicantsController.js
@@ -1,13 +1,22 @@
 import Applicant from "./applicantsModel";
-
-const LIMIT = 8;
+import Person from "../persons/personsModel";
+import Position from "../positions/positionsModel";
+import { LIMIT_APPLICANTS } from "../constants/paginationConstants";
 
 const getAllApplicants = async (req, res, next) => {
   const { page } = req.params;
   try {
     const applicants = await Applicant.findAll({
-      limit: LIMIT,
-      offset: Number(page) * LIMIT,
+      attributes: ["createdAt", "phoneNumber"],
+      limit: LIMIT_APPLICANTS,
+      offset: parseInt(page, 10) * LIMIT_APPLICANTS,
+      include: [
+        {
+          model: Person,
+          attributes: ["firstName", "lastName", "imageUrl", "id"],
+        },
+        { model: Position, attributes: ["name"] },
+      ],
     });
     res.status(200).send(applicants);
   } catch (error) {
