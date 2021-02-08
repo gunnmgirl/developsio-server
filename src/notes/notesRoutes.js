d5b4a7c6c2e27aabe6e96a81573450e91de5fb91
gunnmgirl <48892214+gunnmgirl@users.noreply.github.com>
2021-02-08 15:49:55 +0100

    DEV-114 Adds edit note route (#27)

--- src/notes/notesRoutes.js
+++ src/notes/notesRoutes.js
@@ -9,5 +9,12 @@ const router = express.Router();
 
 router.get("/", isAuth, notesController.getNotes);
 router.post("/", isAuth, validateAddNote, isValid, notesController.addNote);
+router.post(
+  "/edit",
+  isAuth,
+  validateAddNote,
+  isValid,
+  notesController.editNote
+);
 
 export default router;
