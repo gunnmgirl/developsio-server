8843ffd00e1885c248c513a1199e2d086bc4f963
Dzana <gunnmgirl@gmail.com>
2021-02-15 17:16:00 +0100

    DEV-139 Adds update applicant status route

--- src/applicants/applicantsRoutes.js
+++ src/applicants/applicantsRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import isAuth from "../middleware/isAuth";
 
 import applicantsController from "./applicantsController";
+import { validateUpdateApplicantStatus } from "./applicantsValidation";
+import isValid from "../middleware/isValid";
 
 const router = express.Router();
 
@@ -9,5 +11,12 @@ router.get("/all/:page", isAuth, applicantsController.getAllApplicants);
 router.get("/:id", isAuth, applicantsController.getApplicant);
 router.delete("/", isAuth, applicantsController.deleteApplicant);
 router.post("/", isAuth, applicantsController.restoreApplicant);
+router.post(
+  "/status",
+  isAuth,
+  validateUpdateApplicantStatus,
+  isValid,
+  applicantsController.changeApplicantStatus
+);
 
 export default router;
